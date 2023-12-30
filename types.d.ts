2c6fccd07687fbf5a9727e00c8d48e65cf6c1b70
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-30 19:47:50 +0600

    Added new type TransactionBase

--- types.d.ts
+++ types.d.ts
@@ -9,3 +9,28 @@ export type ApiCategory = Omit<Category, 'id'>;
 export interface CategoriesList {
   [id:string]: ApiCategory;
 }
+
+ export interface Transaction {
+  id: string;
+  type: string;
+  category: string;
+  amount: number;
+ }
+
+export interface TransactionMutation {
+  type: string;
+  category: string;
+  amount: string;
+}
+
+export type ApiTransaction = Omit<Transaction, 'id'>;
+
+export interface TransactionsList {
+  [id:string]: ApiTransaction;
+}
+
+export interface TransactionBase {
+  category: [id: string];
+  amount: number;
+  createdAt: string;
+}
