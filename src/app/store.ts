6c0459a17acda14591a6da2a2be91159ba570e03
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-30 13:26:14 +0600

    Moved to store directory

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {categoriesReducer} from '../store/categories/categoriesSlice';
 export const store = configureStore({
   reducer: {
+    categories: categoriesReducer,
   }
 });
 
